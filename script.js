6c89814af4d66581d690dfccf80701cba5a62a29
javlon <javlon_j_tillaev@mail.ru>
2020-06-08 12:09:58 +0300

    dz3

---
+++ script.js
@@ -0,0 +1,30 @@
+let time = +prompt('Foydalanuvchi raqamlarda vaqtni 0 dan 24 gacha')
+switch (time) {
+    case 1 : console.log('сейчас 1 часов ночи ');  break;
+    case 2 : console.log('сейчас 2 часов ночи '); break;
+    case 3 : console.log('сейчас 3 часов ночи '); break;
+    case 4 : console.log('сейчас 4 часов утра  '); break;
+    case 5 : console.log('сейчас 5 часов утра '); break;
+    case 6 : console.log('сейчас 6 часов утра '); break;
+    case 7 : console.log('сейчас 7 часов утра '); break;
+    case 8 : console.log('сейчас 8 часов утра '); break;
+    case 9 : console.log('сейчас 9 часов утра '); break;
+    case 10 : console.log('сейчас 10 часов утра '); break;
+    case 11 : console.log('сейчас 11 часов утра '); break;
+    case 12 : console.log('сейчас 12 часов дня'); break;
+    case 13 : console.log('сейчас 1 часов дня '); break;
+    case 14 : console.log('сейчас 2 часов дня '); break;
+    case 15 : console.log('сейчас 3 часов дня '); break;
+    case 16 : console.log('сейчас 4 часов дня '); break;
+    case 17 : console.log('сейчас 5 часов вечера '); break;
+    case 18 : console.log('сейчас 6 часов вечера '); break;
+    case 19 : console.log('сейчас 7 часов вечера '); break;
+    case 20 : console.log('сейчас 8 часов вечера '); break;
+    case 21 : console.log('сейчас 9 часов вечера '); break; 
+    case 22 : console.log('сейчас 10 часов ночи'); break;
+    case 23 : console.log('сейчас 11 часов ночи'); break;
+    case 24 : console.log('сейчас 12 часов ночи'); break;
+    default: console.log('заданное вами время не существует, пожалуйста изучи как работает время заново в школе');
+    
+    
+}
